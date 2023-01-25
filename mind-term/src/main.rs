016b12e33048f8abf910eb51b175aba0682994c5
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2023-01-26 00:31:51 +0100

    [mind-term] Introduce config, base selections and premise of the CLI.

--- mind-term/src/main.rs
+++ mind-term/src/main.rs
@@ -1,8 +1,29 @@
-use mind::encoding::Node;
-use std::io::stdin;
+mod config;
+
+use config::Config;
+use mind::{encoding, node::Tree};
+use std::fs;
+use structopt::StructOpt;
 
 fn main() {
-  let node: Result<Node, _> = serde_json::from_reader(stdin());
+  let config = Config::from_args();
 
-  println!("{node:#?}");
+  // run on a specific Mind tree
+  if let Some(ref path) = config.path {
+    let tree: encoding::Tree = serde_json::from_str(&fs::read_to_string(path).unwrap()).unwrap();
+    let tree = Tree::from_encoding(tree);
+    with_tree(&config, tree);
+  }
+}
+
+fn with_tree(config: &Config, tree: Tree) {
+  let base_sel = config
+    .base_sel
+    .as_ref()
+    .map(|base_sel| base_sel.split('/').filter(|frag| !frag.trim().is_empty()));
+
+  if let Some(base_sel) = base_sel {
+    let node = tree.get_node_by_path(base_sel);
+    println!("selected node: {node:#?}");
+  }
 }
